8e973bdf07d56b2bb731c7c69078740affc9d1e0
motografa <camilaconsiglio@gmail.com>
2018-02-13 17:59:58 -0200

    Capitulo 14 - JPA com Hibernate. Gerar tabelas do banco a partir de calsse modelo; Inserir e carregar objetos pelo JPA no banco; Buscar vários objetos pelo JPA

--- src/br/com/caelum/tarefas/modelo/Tarefa.java
+++ src/br/com/caelum/tarefas/modelo/Tarefa.java
@@ -2,12 +2,21 @@ package br.com.caelum.tarefas.modelo;
 
 import java.util.Calendar;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+@Entity
 public class Tarefa {
+	
+	@Id
+	@GeneratedValue
 	private Long id;
 	
 	@NotNull
@@ -16,7 +25,8 @@ public class Tarefa {
 	
 	private boolean finalizado;
 	
-	@DateTimeFormat(pattern="dd/MM/yyyy")
+	//@DateTimeFormat(pattern="dd/MM/yyyy")
+	@Temporal(TemporalType.DATE)
 	private Calendar dataFinalizacao;
 	
 	public Long getId() {
